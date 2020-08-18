501a2cb40fcf6b350aa1eef79461cd4ab4455a3e
Gersongs <gersongs@users.noreply.github.com>
2020-08-18 00:43:17 -0300

    Update eulers_number.py

--- eulers_number.py
+++ eulers_number.py
@@ -1,6 +1,6 @@
 import pyformula as pf
 
 #approximation of "e":
-formula = pf.Formula("(1+1/(n))^(n)")
+formula = pf.Formula("(1+1/n)^n")
 formula.setVariable("n","100000")
 print(formula.getValue())
